7f1b3202d6d29e25fa36851b282d3ba27340e0d6
Sophia <phuongmai001@gmail.com>
2018-05-26 12:37:21 -0700

    update lesson 7 exercise

--- bitovi/7-exercise.js
+++ bitovi/7-exercise.js
@@ -1,5 +1,8 @@
 // write the 'new' operator as if it was implemented in JS
 // NEW takes the constructor function and an array of arguments
+// const person = new Person('name');
+// const person = NEW( Person, ['name'] );
+// person.speak();
 
 const Person = function(name) {
   this.name = name;
@@ -11,45 +14,45 @@ Person.prototype.speak = function() {
 
 const NEW = function(constructor, args) {
   // create a new object
-  let obj = {};
+  const obj = {};
   // set proto of our object to the prototype of the constructor
   obj.__proto__ = constructor.prototype;
   // call constructor with our object as the context
-  constructor(obj);
+  constructor.call(obj, args); // OR constructor.apply(obj, args);
   // return our new object
   return obj;
 };
 
-// const person = new Person('name');
-
-// const person = NEW( Person, ['name'] );
-
-// person.speak();
-
 // Example: 
-// const justin = new Person('Justin');
-// justin.speak(); // Hello Justin
+const justin = new Person('Justin'); // using regular 'new' operator
+justin.speak(); // Hello Justin
 
-// const allina = new Person('Allina');
-// allina.speak(); // Hello Allina
+const allina = NEW(Person,'Allina'); // use the NEW fn we just create above
+allina.speak(); // Hello Allina
+
+const Sophia = NEW(Person, ['Sophia']);
+Sophia.speak(); // Hello Sophia
 
 
+// -----------
 // write the instanceof operator as if it was implemented in JS
 // check the proto chain for the constructor's prototype
 
-const INSTANCEOF = function(instance, constructor) {
-  if (instance && constructor) {
-    // walk up obj's proto chain until the prototype of our constructor is found
-    if (instance.__proto__ === constructor.prototype) {
+const INSTANCEOF = function(object, constructor) {
+  while (object.__proto__) {
+    if (object.__proto__ === constructor.prototype) {
       return true;
     }
+    object = object.__proto__;
   }
   return false;
 };
 
-person = new Person('Bonika');
+const person = NEW(Person, 'Bonika');
+
 console.log(INSTANCEOF(person, Person));
 
+// Extra:
 // `const a = [];` // same as const a = new Array();
 // a instanceof Array; // this will return `true`
 // a.__proto__ === Array.prototype; // this will also `true`

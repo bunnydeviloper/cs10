30433f62e8a6b9659b3a99d88e982cd8e7a4d24d
Sophia <phuongmai001@gmail.com>
2018-06-04 15:20:15 -0700

    update ex9

--- bitovi/009-exercise.js
+++ bitovi/009-exercise.js
@@ -4,12 +4,23 @@ console.log(document.getElementById('ironman'));
 
 // ----- Exercise: creating the $ function
 const $ = function(selector) {
-  const arr = [];
-  arr.length = 0;
-  arr.push.apply(this, items);
-
-  return //array-like object
+  const elements = document.querySelectorAll(selector);
+  for (let i=0; i< elements.length; i++) {
+    const node = elements[i];
+    this[i] = node; // can you combine two lines into 'this[i] = elements[i]' ???
+  }
+  this.length = elements.length;
 };
+
+// test
+const listOfHeros = new $('ul li'); // const $li = new $("ul li");
+console.log(listOfHeros); // $ {0: li, 1: li, 2: li, Length: 3} // can expand to see li elements
+console.log(listOfHeros instanceof $); // true
+console.log(listOfHeros.length); // 3
+console.log(listOfHeros[0]); // <li><a href='#ironman">Ironman</a></li>
+
+
+// $li.html();
 
 /*
 @.extend = function(target, object) {
@@ -40,10 +51,3 @@ const $ = function(selector) {
 });
 */
 
-
-
-// test
-//const ironMan = new $('#ironMan');
-//ironMan instance of $ // true
-//ironMan.length // 1
-//ironMan[0] // <ul#ironMan>

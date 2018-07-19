e6f02f30c1cc7f02349972bcd5be85d626d3eddd
Sophia <phuongmai001@gmail.com>
2018-07-18 22:08:35 -0700

    add temp insert file

---
+++ Khan/Algorithms/005-insertion-sort/insert.js
@@ -0,0 +1,11 @@
+const insert = (array, rightIndex, value) => {
+  // do something
+};
+
+const array = [3, 5, 7, 11, 15, 2, 9, 6];
+
+insert(array, 4, 2);
+console.log(`Array after inserting 2: ${array}`);
+
+insert(array, 5, 9);
+console.log(`Array after inserting 9: ${array}`);

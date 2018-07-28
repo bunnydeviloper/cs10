105eb518ae05dc3353ed7f0d20a9e8194918b9cd
Sophia <phuongmai001@gmail.com>
2018-07-28 10:46:27 -0700

    update exercise

--- Udacity/part4-OOP-javascript/009-exercise.js
+++ Udacity/part4-OOP-javascript/009-exercise.js
@@ -1,20 +1,27 @@
+// Parent function
 function Animal (name) {
   this.name = name;
 }
 Animal.prototype.walk = function () {
   console.log(`${this.name} walks!`);
 }
+
+// Child function
 function Cat (name) {
   Animal.call(this, name); // we don't want to create a whole new obj, just need the initialization
   this.lives = 9;
 }
 Cat.prototype = Object.create(Animal.prototype);
+
 Cat.prototype.constructor = Cat;
 
 Cat.prototype.meow = function () {
   console.log('Meow!');
 }
+
 const kitty = new Cat('Kitty');
+console.log(kitty instanceof Animal); // true
+
 kitty.meow(); // Meow!
 kitty.walk(); // Kitty walks!
 console.log(kitty.name); // Kitty

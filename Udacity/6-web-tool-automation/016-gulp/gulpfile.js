d27d35564aa2a3564da37d0eb2a0abb69c6a7cee
Sophia Mai <phuongmai001@gmail.com>
2018-07-31 23:09:46 -0700

    update gulp fn for watching for changes

--- Udacity/6-web-tool-automation/016-gulp/gulpfile.js
+++ Udacity/6-web-tool-automation/016-gulp/gulpfile.js
@@ -5,17 +5,22 @@ var autoprefixer = require('gulp-autoprefixer');
 // Since your task might contain asynchronous code you have to signal gulp when your task has finished executing (= "async completion").
 // to fix error, gulp automatically passes a callback function to your task as its first argument. Just call that function when you're done
 gulp.task('default', function(cb) {
-	console.log('hello world!');
+  console.log('hello world!');
+  // watch: 1st arg is the files that we are watching for changes
+  // 2nd arg can be a call back or an array of sets of tasks
+  gulp.watch('sass/**/*.scss', ['styles']);
+
   cb();
 });
 
 gulp.task('styles', function(cb) {
   // tell gulp what files we want to work with: look in sass folder (and any sub directories) with files ending in *.scss
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError)) // must have error fn
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError)) // must have error fn
+    .pipe(autoprefixer({
+      // specify you want to prefix the last two versions of all browsers
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'));
   cb();
 });

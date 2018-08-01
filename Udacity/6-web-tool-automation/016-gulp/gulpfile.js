25c3f6991f7bf5683f19501ebabc567264a474b9
Sophia Mai <phuongmai001@gmail.com>
2018-07-31 22:36:25 -0700

    update gulpfile

--- Udacity/6-web-tool-automation/016-gulp/gulpfile.js
+++ Udacity/6-web-tool-automation/016-gulp/gulpfile.js
@@ -1,6 +1,16 @@
-const gulp = require('gulp');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-  // do useful stuff here
-  console.log('Hello with Gulp');
+	console.log('hello world!');
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
 });

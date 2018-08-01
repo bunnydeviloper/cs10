2ff3806bb3d60c67b184e72bdf7d09e166e21b60
Sophia Mai <phuongmai001@gmail.com>
2018-07-31 22:49:04 -0700

    update gulpfile with callback for async

--- Udacity/6-web-tool-automation/016-gulp/gulpfile.js
+++ Udacity/6-web-tool-automation/016-gulp/gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
+// Since your task might contain asynchronous code you have to signal gulp when your task has finished executing (= "async completion").
+// to fix error, gulp automatically passes a callback function to your task as its first argument. Just call that function when you're done
+gulp.task('default', function(cb) {
 	console.log('hello world!');
+  cb();
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function(cb) {
   // tell gulp what files we want to work with: look in sass folder (and any sub directories) with files ending in *.scss
 	gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError)) // must have error fn
@@ -14,4 +17,5 @@ gulp.task('styles', function() {
 			browsers: ['last 2 versions']
 		}))
 		.pipe(gulp.dest('./css'));
+  cb();
 });

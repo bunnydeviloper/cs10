4f6ac40aa50b185c33dcd07b8c2b6757dbfb88d5
Sophia Mai <phuongmai001@gmail.com>
2018-08-02 01:31:17 -0700

    update example

---
+++ Udacity/7-unit-test/022-test/src/AddressBook.js
@@ -0,0 +1,12 @@
+function AddressBook() {
+  // do something
+  this.contacts = [];
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.getContact = function(index) {
+  return this.contacts[index];
+}

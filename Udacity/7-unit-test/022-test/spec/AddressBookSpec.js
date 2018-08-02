0019cde4079919b74f697e8581a38efc8a83ff9a
Sophia Mai <phuongmai001@gmail.com>
2018-08-02 01:52:07 -0700

    update delete fn

--- Udacity/7-unit-test/022-test/spec/AddressBookSpec.js
+++ Udacity/7-unit-test/022-test/spec/AddressBookSpec.js
@@ -2,8 +2,15 @@ describe('Address Book', function() {
   it('should be able to add contact', function() {
     const addressBook = new AddressBook();
     const thisContact = new Contact();
-
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+
+  it('should be able to delete contact', function() {
+    const addressBook = new AddressBook();
+    const thisContact = new Contact();
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+    expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });

4d3f6d80b7f470bf86d7a2e332653e95b8cc89c1
Sophia <phuongmai001@gmail.com>
2018-08-03 19:20:10 -0700

    update cat clicker files

--- Udacity/8-front-end-app/catClicker/js/app.js
+++ Udacity/8-front-end-app/catClicker/js/app.js
@@ -1,2 +1,29 @@
-let count = 1;
 
+/*
+window.onload(function() {
+  start();
+});
+*/
+
+let count = 0;
+let meow = '';
+
+const img = document.getElementById('catimg');
+const displayCount = document.getElementById('count');
+displayCount.innerHTML = count;
+const displayMeow = document.getElementById('meow');
+displayMeow.innerHTML = meow;
+
+
+img.addEventListener('click', function() {
+  //do sth
+  count++;
+  displayCount.innerHTML = count;
+  meow += 'meowww ';
+  displayMeow.innerHTML = meow;
+
+}, false);
+
+const start = () => {
+  count = 0;
+}

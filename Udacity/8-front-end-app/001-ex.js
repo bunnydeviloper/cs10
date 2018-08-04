554c612a8c4736b0b2dafc0f3e3771874620b78e
Sophia <phuongmai001@gmail.com>
2018-08-03 22:25:09 -0700

    add closure exercise

---
+++ Udacity/8-front-end-app/001-ex.js
@@ -0,0 +1,39 @@
+// clear the screen for testing
+document.body.innerHTML = '';
+document.body.style.background="white";
+
+var nums = [1,2,3];
+
+// Let's loop over the numbers in our array
+for (var i = 0; i < nums.length; i++) {
+
+  // This is the number we're on...
+  // This variable keeps changing every time we iterate!
+  //  It's first value is 1, then 2, then finally 3.
+  var num = nums[i];
+
+  // We're creating a DOM element for the number
+  var elem = document.createElement('div');
+  elem.textContent = num + ' click me';
+
+  // On click...
+  elem.addEventListener('click', function() {
+
+    // ... and when we click, alert the value of `num`
+    alert(num);
+
+    // Specifically, we're alerting the num variable
+    // that's defined outside of this inner function.
+
+    // Each of these inner functions are pointing to the
+    // same `num` variable... the one that changes on
+    // each iteration, and which equals 3 at the end of
+    // the for loop.  Whenever the anonymous function is
+    // called on the click event, the function will
+    // reference the same `num` (which now equals 3).
+
+  });
+
+  // finally, let's add this element to the document
+  document.body.appendChild(elem);
+};

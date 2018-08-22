7514e6623272e088fede610bb4ac600b93ca66b1
Sophia Mai <phuongmai001@gmail.com>
2018-08-22 15:03:39 -0700

    add lesson 1 for 1mwtt

---
+++ 1MWTT/hourofcode-startprogramming.js
@@ -0,0 +1,94 @@
+HOUR OF CODE: START PROGRAMMING
+~~~~~ GETTING TO KNOW YOU ~~~~~
+1. What is your name?
+"sophia"
+
+2. Discover the length
+"sophia".length
+
+3. Basic Math
+3+4
+
+4. Numbers and more
+You can use the command line to do basic math operations: + - / * %
+
+5. Error: does not compute!
+Try to confuse the interpreter by using a word it doesn''t know, like eggplant.
+It will give you a ReferenceError: eggplant is not defined
+
+~~~~~ WHY LEARN PROGRAMMING? ~~~~~
+6. Editor and comments
+Anything after a // is called a comment and is ignored by the computer.
+You can also temporarily disable lines of code by commenting them out, or adding // in front of them
+
+7. What am I learning?
+What can we use JavaScript for?
+
+ make websites respond to user interaction
+ build apps and games (e.g. blackjack)
+ access information on the Internet (e.g. find out the top trending words on Twitter by topic)
+ organize and present data (e.g. automate spreadsheet work; data visualization)
+
+8. Interactive JavaScript
+confirm("I feel awesome!");
+=> pop up box, similar to alert
+=> this expression will return true
+
+9. What is programming?
+Programming is like writing a list of instructions to the computer so it can do cool stuff with your information.
+
+To do any of these actions, the program needs an input. You can ask for input with a prompt.
+
+prompt("What is your name?")
+=> this expression will return the value of user''s input
+
+10. Data Types I & II: Numbers and Strings
+a. numbers are quantities, just like you''re used to. You can do math with them. >> withnout quotes
+b. strings are sequences of characters, like the letters a-z, spaces, and even numbers >> with "quotes"
+
+11. Data Type III: Booleans
+A boolean can have only two values, true or false
+
+~~~~~ MAKE THE COMPUTER THINK! ~~~~~
+12. Using console.log
+This is commonly called printing out.
+console.log(2 * 5)
+console.log("Hello")
+
+13. Comparisons
+List of comparison operators:
+> Greater than
+< Less than
+<= Less than or equal to
+>= Greater than or equal to
+=== Equal to
+!== Not equal to
+
+14. Decisions, Decisions
+You can use comparisons plus booleans to decide whether a block of code should run.
+This is called an if statement or conditional statement.
+
+if ( 100 > 2 ){console.log("You are good at math!");}
+
+15. Computers are smart
+if ( 7 > 9 )
+{ alert("Let's go down the first road!");}
+else
+{    // What should we do if the condition is false? Fill in here:
+    console.log('Check your math again!');}
+
+16. Computers aren''t that smart
+Syntax needs to be in exactly the right place for the computer to understand the code () {} [] ; , ...
+
+~~~~~ MID-LESSON BREATHER ~~~~~
+17. Mid-lesson breather [review]
+
+~~~~~ VARIABLES ~~~~~
+18. Variables
+declare a variable:
+var varName = "data";
+
+19. More variable practice
+var myName = "Steve Jobs";
+myName.substring(0,5)
+>> "Steve Jobs".substring(0,5) >> Steve

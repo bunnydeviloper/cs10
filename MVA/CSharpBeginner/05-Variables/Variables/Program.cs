06e22003e246c1997b3c4a5c1a25e4f06bc84c35
Sophia Mai <phuongmai001@gmail.com>
2018-11-21 17:38:59 -0800

    add variable lesson in c#

---
+++ MVA/CSharpBeginner/05-Variables/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a;
+            int b;
+
+            a = 10;
+            b = a + 2;
+            Console.WriteLine(b);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            // string myFirstName;
+            // myFirstName = Console.ReadLine();
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName;
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}

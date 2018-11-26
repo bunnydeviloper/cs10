6406fcc3f359e0627f64a5d558112bd33052390e
Sophia Mai <phuongmai001@gmail.com>
2018-11-26 13:18:47 -0800

    add lesson 23; add timer practice

---
+++ MVA/CSharpBeginner/23-TimerExample/TimerExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // events allow developer to response by handling key moments in the lifecycle of application execution 
+            // eg: on application starts up, the static void Main is handling that event
+
+            Timer myTimer = new Timer(2000);
+            myTimer.Elapsed += MyTimer_Elapsed; // adding an event handler
+            myTimer.Elapsed += MyTimer_Elapsed1; // adding another event handler
+
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the red event.");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1; // removing an event handler
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            // throw new NotImplementedException();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // throw new NotImplementedException();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}
